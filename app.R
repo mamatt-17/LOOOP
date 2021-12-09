41a98d6672cf1c9ddc6825e8613b9a8e668cd427
Monica Matt <mamatt@upstatefreshwater.org>
2021-12-09 17:43:30 -0500

    Attempts at UI

--- app.R
+++ app.R
@@ -5,119 +5,33 @@
 
 
 # Load packages and Set Working Directory----
-my_packages <- c("lubridate", "plyr", "openxlsx", "dplyr","ggpubr",  "tidyr", "shiny","ggplot2")
+my_packages <- c("lubridate", "plyr", "openxlsx", "dplyr","ggpubr",  "tidyr", "shiny","ggplot2","leaflet")
 lapply(my_packages, require, character.only = TRUE)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Read data and rename Inputs----
-data <- read.csv("shiny_data.csv")
-data$Input <-as.factor(data$Input)
-levels(data$Input) <-list("1" = "Input 1", "19" = "Input 19", "20" = "Input 20", "32"= "Input 32")
+coord <-readOGR(dsn="Coordinates.shp",layer = "station.co", encoding = "UTF-8")
+mcoord<-read.csv("Coordinates.csv")
+coordinates <- SpatialPointsDataFrame(mcoord[,c('Longitude', 'Latitude')] , mcoord)
 
-# Define UI (user interface) for application that draws a plot based on selected inputs----
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("DeRuyter Tributaries 2020"),
-  
-  # Create a sidebar that will contain our interactive features
-  fluidRow(
-    column(width = 3,wellPanel(
-      # Create a select box with DeRuyter Lake Inputs as our choices 
-      selectInput(inputId = "trib", "Input No.",
-                  choices = c("1", "19", "20", "32"),
-                  selected = 1),
-      # Create another select box with Lab analyses as our choices
-      radioButtons(inputId = "nutrient", "Lab Parameter",
-                   choices = c("TN","TP","TSS"),
-                   selected = "TN"))),
-    
-    # On the same line as the selection features, add a box that will give information of point when clicked
-    fluidRow(
-      column(width = 7,
-             h4("Points near click"),
-             verbatimTextOutput("click_info")))
-  ),
-  # Based on selection features, show a plot of the data for that tributary upon selection and be able to click on points
-  fluidRow(
-    column(width = 12,
-           plotOutput("nutrientPlot", height = 350,
-                      click = "plot_click")
-    )),
-  fluidRow(
-    column(width = 7,offset = 5,
-           img(src = "UFI_HighResLogo.png", height =100, width =190)
-    ))
+
+ui <-fluidPage(
+  headerPanel("LOOOP - Lake Ontario, Oneida, Onondaga Program"),
+  leafletOutput("map")
 )
-
-
-# Define server logic required to draw the plot----
-server <- function(input, output) {
+server <- function(input, output, session){
   
-  # Create the output plot (as named in our UI)
-  output$nutrientPlot <- renderPlot({
-    # Create a reactive data frame that responds to interactive selections
-    Input_filter<- reactive({
-      data <-data %>% filter(
-        Input == input$trib) %>%
-        filter(
-          Test == input$nutrient)
-    })
-    
-    # Create dynamic y axis label based on which nutrient is selected
-    if(input$nutrient == "TN"){
-      ylabel <- "TN (µg/l)"
-    } else if(input$nutrient=="TP"){
-      ylabel <- "TP (µg/l)"
-    } else if(input$nutrient == "TSS")
-      ylabel <- "TSS (mg/l)"
-    
-    # Create dynamic y axis limits based on which nutrient is selected
-    if(input$nutrient == "TN"){
-      limits <- c(100,2200)
-    } else if(input$nutrient=="TP"){
-      limits <- c(0,550)
-    } else if(input$nutrient == "TSS")
-      limits <- c(0,90)
-    
-    # Create dynamic y axis breaks based on which nutrient is selected
-    if(input$nutrient == "TN"){
-      breaks <- seq(100,2200,500)
-    } else if(input$nutrient=="TP"){
-      breaks <- seq(0,550,100)
-    } else if(input$nutrient == "TSS")
-      breaks <- seq(0,90,15)
-    
-    # Actual plotting code
-    ggplot(data = Input_filter(),aes(x = Average.q,Result, color = "black"))+ # Use the filtered dataframe, x = Flow, y = Result of whatever nutrient is selected
-      geom_point(size = 4, color = "black") + # Size and color of scatterplot
-      theme_minimal()+ 
-      scale_y_continuous(name = ylabel, # dynamic y label
-                         limits = limits, # dynamic limits
-                         breaks = breaks, # dynamic breaks
-                         expand = c(0.1,0.1))+ # Slight cushion around axis so that limits are not "absolutes"
-      scale_x_continuous(name = "Flow (cfs)", # X label
-                         limits = c(0,3), # x axis limits
-                         breaks = seq(0,3,.25), # Ticks from 0 to 3, every 0.25
-                         expand = c(.01,.1))+ # Slight cushion around axis so that limits are not limiting data from being shown
-      geom_smooth(method = "lm", se = FALSE, color = "blue")+ # Add a linear regression line and make it blue
-      stat_regline_equation(label.x.npc = 0.02,label.y.npc = "top", # Add the Regression equation 2% from left and at the top of the plot
-                            aes(label = paste(..eq.label.., ..adj.rr.label.., sep = "~~~")), color = "black", size = 8)+ # Paste equation and adjusted R squared
-      stat_cor(label.x.npc = 0.02, label.y.npc = 0.8, aes(label = paste(..p.label..)), color = "black", size = 8)+ # Paste p value 2% from left and 80% from bottom of plot
-      theme(
-        legend.position = "none", # Do not show legend
-        panel.border = element_rect(color = "black", fill = NA, size = 1), # Create a border around the plot
-        axis.ticks = element_line(color = "black", size = 1), # Add tickmarks so they can be seen
-        axis.text = element_text(size = 14), # Make axis text size 14
-        axis.title = element_text(size = 14)
-      )
-  })
-  # Print the data frame for the nearest selected points
-  output$click_info <- renderPrint({
-    nearPoints(data,input$plot_click, addDist = F, threshold = 3)
-  })
-}
+  # Loading modal to keep user out of trouble while map draws...
+  showModal(modalDialog(title="MAP LOADING - PLEASE WAIT...","Please wait for map to draw before proceeding.",size="l",footer=NULL))
 
-# Run the application ----
+  # Draw map
+  output$map <-renderLeaflet({
+    leaflet %>%
+      addTiles() %>%
+      addCircles(lat = mcoord$lat,
+                 lng = mcoord$long)
+
+  })
+  }
+
+## run app----
 shinyApp(ui = ui, server = server)
-
--- looop_data.R
+++ looop_data.R
@@ -46,11 +46,46 @@ buoy.coord <-as.data.frame(unique(b$station.code))
 names(buoy.coord) <-c("station.code")
 buoy.coord$lat <-c(43.240093,43.44995,43.205123,43.231178,43.193388,43.139376,43.147516,43.103883,43.100510,43.108055)
 buoy.coord$long <- c(-76.147568,-76.50349,-76.269799,-76.309791,-76.279911,-76.238025,-76.314771,-76.445725,-76.499537,-76.475456)
-#buoy.coord <-as.data.frame(unique(b$station.code)) # Check that the buoys are in the right river
+#unique(b[c("system.code","station.code")]) # Check that the buoys are in the right river
 
-    # Remove time_id and data_id
-b <- b %>% select(!c(time_id,data_id))
+    # Round depth to nearest meter (determine if there's profiles, discrete depths, or typos)
+b <- b %>%
+    mutate(r.depth = lapply(depth..m.,
+                            function(x)
+                                round(x, digits = 0)))
+# unique(b[c("station.code","r.depth")]) # Typos= B211.6554, B224.-9999; Discrete = B148 @ 2; Profiles or multiple depths = B211, B143, B22, B224, B266, B317, B409, B430, CROSS
+b$r.depth <-as.numeric(as.character(b$r.depth))
     
+    # Fixing typo
+b$r.depth[b$station.code=="B211"& b$r.depth=="6554"]<- 3 # changing rounded depth to similar depth of other dates
+
+    # Cleaning data frame
+b <- b %>% select(!c(time_id,data_id, ORP..mV.,timeperiod)) # Remove unnecessary columns
+names(b) <- c("System", "Station", "Date", "Abs.Time", "Abs.Depth","Temp","SC","pH","DO","Tn","Chl","Datetime","Time","Depth") # Rename remaining columns
+
+#cbind(lapply(lapply(b, is.na), sum)) # Identifying columns that have NAs
+b1 <- b[!(is.na(b$Temp)) | !(is.na(b$SC)) | !(is.na(b$pH)) | !(is.na(b$DO))| !(is.na(b$Tn))| !(is.na(b$Chl)),] # Removing rows that have no data at all (Some NAs left within single columns for faulty probes, etc.)
+
+s.check<- plyr::ddply(b1, .variables = .(Station), plyr::summarize,
+                      t = mean(Temp, na.rm = T),
+                      tmin = min(Temp, na.rm = T),
+                      tmax = max(Temp, na.rm = T),
+                      sc = mean(SC, na.rm = T),
+                      scmin = min(SC, na.rm = T),
+                      scmax = max(SC, na.rm = T),
+                      pH = mean(pH, na.rm = T),
+                      pHmin = min(pH, na.rm = T),
+                      pHmax = max(pH, na.rm = T),
+                      DO = mean(DO, na.rm = T),
+                      DOmin = min(DO, na.rm = T),
+                      DOmax = max(DO, na.rm = T),
+                      Tn = mean(Tn, na.rm = T),
+                      Tnmin = min(Tn, na.rm = T),
+                      Tnmax = max(Tn, na.rm = T),
+                      Chl = mean(Chl, na.rm = T),
+                      Chlmin = min(Chl, na.rm = T),
+                      Chlmax = max(Chl, na.rm = T))
+
     # C. Lake Ontario----
 
     # D. Oneida Lake----   
