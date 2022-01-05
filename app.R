0d658ec172830a13e5202c8c428b8d630a41fd25
Monica Matt <mamatt@upstatefreshwater.org>
2022-01-05 16:21:28 -0500

    Addition of Topic texts
    Topic text markdowns added. Notes/Instructions added for each line of code in app. General organization of folder

--- app.R
+++ app.R
@@ -1,15 +1,16 @@
 # Shiny File for running LOOOP page
-# You can run the application by clicking the 'Run App' button above. 
-# Arrow next to text allows you to run in Viewer or external window.
+# You can run the application by clicking the 'Run App' button above; large application so it may not load
 # Note, when the application is running you have to press the Stop button in order to run additional code.
+
+# Use shinyApp() to print application; images will not load using this function
 
 
 # Load packages and Set Working Directory----
-my_packages <- c("lubridate", "plyr", "openxlsx", "dplyr","ggpubr",  "tidyr", "shiny","ggplot2","leaflet", "ggvis", "RSQLite")
+my_packages <- c("lubridate", "plyr", "openxlsx", "dplyr","ggpubr",  "tidyr", "shiny","ggplot2","leaflet", "ggvis", "RSQLite","knitr")
 lapply(my_packages, require, character.only = TRUE)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Setup for Application----
+# Initial Setup for Application----
 
 # Load data files from initial data workup file (looop_data)
 source("looop_data.R")
@@ -17,20 +18,22 @@ source("looop_data.R")
 
 # Create User Interface (UI)----
 ui <-fluidPage(
-
 # Title that will appear at top of page
   navbarPage(title = div(
     #style = "padding: 1px 0px; width: '100%'",
-    img(src = "~/logo_looop.png", width = "20px",height = "20px"),
+    img(src = "logo_looop.png", width = "20px",height = "20px"),
     "LOOOP-Lake Ontario, Oneida, Onondaga Program"
     ), 
 
-# First navigational tab, icons from free selection by fontawesome.com
+  # Data Explorer Interactive Application tab (sub-tabs on side panel)----
   tabPanel("Data Explorer", icon = icon("chart-bar"), 
 
+# Adding in side-panel navigational sub-tabs
+navlistPanel("Data Explorer",
+# First side-panel navigational tab (Application)
+             tabPanel("Application",
 # Setting up interface using fluid grid system (12 columns across)
-fluidRow(
-
+                      fluidRow(
 # In columns 1-5, map of area with data points will appear
   column(5,
          leafletOutput("mymap")
@@ -65,46 +68,63 @@ column(4,
        verbatimTextOutput("depths")
        )
 )
-), # End of Data tab
-# How to Use Data Explorer tab
-tabPanel("How to Use Explorer", icon = icon("question-circle"),
-), #End of How-To tab
+), # End of Application Sub-tab
 
-# Lake Characteristics navigational (expandable) tab
+# Second side-panel navigational tab (User's Guide)
+tabPanel("User's Guide",
+         h4(p("Coming Soon"))
+         ), # End of How-To Sub-tab
+
+# Third side-panel navigational tab (Meta Data)
+tabPanel("Metadata",
+         h4(p("Coming Soon"))
+         ), # End of Meta sub-tab
+# Set side-panel width (2) and Nav list panel (10) widths using fluid grid system
+fluid = TRUE, widths = c(2,10))), # End of Data Explorer tab
+
+  # Lake Characteristics navigational (expandable) tab----
 navbarMenu("Lake Characteristics", icon = icon("water"),
 # Options beneath "Lake Characteristics"
            tabPanel("Lake Ontario",
-# Anything related to Lake Ontario will go here
+                    h4(p("Coming Soon"))
                     ),
            tabPanel("Oneida Lake",
-# Anything related to Oneida Lake will go here
+                    h4(p("Coming Soon"))
                     ),
            tabPanel("Onondaga Lake",
-# Anything related to Onondaga Lake will go here
+                    h4(p("Coming Soon"))
                     )
            ), # End of Lake Characteristics tab
 
-# Topics navigational (expandable) tab
+  # Topics navigational (expandable) tab----
 navbarMenu("Topics", icon = icon("lightbulb"),
 # Options beneath "Topics"
            tabPanel("Great Lakes Geomorphology",
-                    ),
+# Read R markdown file that contains text and images
+                    includeMarkdown("StaticPosts/Geomorph.Rmd")),
            tabPanel("Lake Levels",
-                    ),
+                    includeMarkdown("StaticPosts/LakeLevels.Rmd")),
            tabPanel("Land Use",
-                    ),
+                    includeMarkdown("StaticPosts/LandUse.Rmd")),
            tabPanel("Fisheries",
-                    ),
+                    h5(p("Coming Soon"))),
            tabPanel("Effects of Pollution",
-                    )
-           ),
+                    includeMarkdown("StaticPosts/Pollution.Rmd"))
+           ), # End of Topics tab
+
+  # About LOOOP tab----
 tabPanel("About LOOOP", icon = icon("info-circle"), 
 fluidRow(column(6,
-                h5(p("This is just some practice text.")
-                   )
-                )
+                includeMarkdown("StaticPosts/About.Rmd")),
+         column(6,
+                img(src = "~/logo_looop.png", width = "20px",height = "20px"))
          )
-) # End of About tab
+), # End of About tab
+
+  # Credits and Privacy Policy tab----
+tabPanel("Credits, Policy and Contact Information", icon = icon("question-circle"),
+         h4(p("Coming Soon"))
+) #End of Policy tab
 ) # End of NavBarpage
 ) # End of Fluid Page
 
@@ -147,4 +167,4 @@ output$depths <- renderPrint({input$depth})
 }
 ## Run app----
 shinyApp(ui = ui, server = server)
-runApp()
+#runApp()
